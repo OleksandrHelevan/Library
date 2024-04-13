00a8712c5f347179e24511de8774523768f7726c
OleksandrHelevan <oleksandrhelevan@gmail.com>
2024-04-13 22:16:42 +0300

    Double Linked List

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "SinglyLinkedList.h"
+#include "DoubleLinkedList.h"
 
 using namespace std;
 
@@ -45,8 +46,48 @@ void SingleList (){
     else cout<<"List don`t have 2"<<endl;
     cout<<"Size of list: "<<A.getSize()<<endl;
     cout<<A[3]-A[2];
+}
 
+void DoubleList(){
+    DoubleLinkedList <int> B;
+    Shtrix();
+    if(B.isEmpty())
+        cout<<"List is empty"<<endl;
+    Shtrix();
+    B.insertAtBeginning(3);
+    B.insertAtBeginning(2);
+    B.insertAtBeginning(1);
+
+
+    if(!B.isEmpty())
+        cout<<"List isn`t empty"<<endl;
+    cout<<B;
+    Shtrix();
+    B.insertAtEnd(6);
+    B.insertAtEnd(5);
+    B.insertAtEnd(7);
+
+    cout<<B;
+    Shtrix();
+
+    B.removeFromEnd();
+    B.removeFromBeginning();
+    cout<<B;
+    Shtrix();
+
+    B.removeAtIndex(1);
+    B.insertAtIndex(1,5);
+    cout<<B;
+    Shtrix();
+
+
+    if (B.search(2))
+        cout<<"List has 2"<<endl;
+    else cout<<"List don`t have 2"<<endl;
+    cout<<"Size of list: "<<B.getSize()<<endl;
+    cout<<B[3]-B[2];
 }
 int main() {
 //SingleList();
+//DoubleList();
 }
